ea978ab18c0b501a714c7958d2de48be2024c246
HarisAhmed777 <ahmedharis371@gmail.com>
2024-12-20 18:06:18 +0530

    added:logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../context'
 import ToggleButton from './ToggleButton/ToggleButton';
+import LogoBlack from '../components/images/Logoblack.jpg';
+import LogoWhite from '../components/images/Logowhite.jpg';
 
 function Header() {
     const {isLight} = useContext(ThemeContext);
   return (
     <div className={`${isLight?'lightmode':'darkmode'} flex items-center justify-between px-10 py-6`}>
-      <h2>Logo will come here</h2>
+      <img src={isLight?LogoWhite:LogoBlack} alt="" className='h-16' />
       <ToggleButton/>
     </div>
   )
